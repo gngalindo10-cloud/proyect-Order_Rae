5b11b907f72b241043dea9a53728d4fc2f4b073a
Gonzalo Niño Galindo <g.n.galindo10@gmail.com>
2025-09-02 20:29:05 -0500

    Conexión de la bd para los modelos y colección de Inventario en web.php

--- routes/web.php
+++ routes/web.php
@@ -1,10 +1,15 @@
 <?php
 
+use App\Http\Controllers\Inventario\InventarioController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::middleware(['auth:sanctum',config('jetstream.auth_session'),'verified'])
+->resource('Inventario', InventarioController::class)
+->names('Inventario');
 
 Route::middleware([
     'auth:sanctum',
